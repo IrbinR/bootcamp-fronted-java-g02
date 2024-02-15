18de6edb04c486055fefcc0c2b4a20b443ceca5d
IrbinR <salazar.irbin@gmail.com>
2024-02-14 19:59:27 -0500

    semana03, dia02, trabajando con javascript

---
+++ semana02/dia02-javascript-basico/app.js
@@ -0,0 +1,86 @@
+console.log("Hello Javascript!")
+
+// Esto es un comentario en una linea
+/*
+    Esto
+    es 
+    un
+    comentario
+    de
+    múltiples
+    lineas
+ */
+
+// TIPOS DE DATOS
+
+// PRIMITIVOS: Number, String, Boolean (true, false), undefined, null
+
+// NUMBER
+const numero1 = 20
+const numero2 = 20.50
+const numero3 = -36
+
+console.log(numero1)
+console.log(numero2)
+console.log(numero3)
+
+console.log(typeof numero1)
+console.log(typeof numero2)
+console.log(typeof numero3)
+
+// STRING o cadena de texto
+
+const alumna = 'Claudia'
+const alumno = 'Marco'
+const numero4 = "200"
+const numero5 = 200;
+
+console.log(alumna)
+console.log(alumno)
+console.log(numero4)
+console.log(numero5)
+
+console.log(typeof alumna)
+console.log(typeof alumno)
+console.log(typeof numero4)
+console.log(typeof numero5)
+
+// BOOLEAN -> false o true
+
+const tieneDescuento = true
+
+console.log(tieneDescuento)
+console.log(typeof tieneDescuento)
+
+// UNDEFINED
+
+let nombre
+
+console.log(nombre)
+console.log(typeof nombre)
+
+// NULL
+
+let apellido = null
+
+console.log(apellido)
+console.log(typeof apellido)
+
+//Object
+// Function
+// BigInt
+// Symbol
+
+// VARIABLES Y CONSTANTES
+
+// ESCMACRIPT 5 == ES5
+// ESCMACRIPT 6 == ES6
+
+// var -> ES5 (Forma de declarar una variable)
+var nombreDeMiVariable = 'un valor'
+
+// CONST -> ES6 (El uso de const es lo recomendado)
+
+const edad = 55
+const _edad = 25
+const edadDeMiHijo = 15
